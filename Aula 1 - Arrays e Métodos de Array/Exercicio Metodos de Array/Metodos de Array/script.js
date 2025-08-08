3a8dc70bd5f81a335a171044367c0b614fcaf656
Nicolas Tetsuo Kimura <nicolas.t.kimura@gmail.com>
2025-08-07 22:40:44 -0300

    [Commentary] Class Notes

--- Aula 1 - Arrays e Métodos de Array/Exercicio Metodos de Array/Metodos de Array/script.js
+++ Aula 1 - Arrays e Métodos de Array/Exercicio Metodos de Array/Metodos de Array/script.js
@@ -1,47 +1,64 @@
 function executarPush() {
   const array = ['🍎', '🍌'];
+  array.push("🍇");
   const resultado = array;
   document.getElementById('resultado-push').textContent = resultado;
+  //Push adiciona no final do array.
 }
 
 function executarPop() {
   const array = ['🍎', '🍌', '🍇'];
+  array.pop();
   const resultado = array;
   document.getElementById('resultado-pop').textContent = resultado;
+  //Remove o último item.
 }
 
 function executarShift() {
   const array = ['🍎', '🍌', '🍇'];
+  array.shift();
   const resultado = array;
   document.getElementById('resultado-shift').textContent = resultado;
+  //Remove o primeiro item.
 }
 
 function executarUnshift() {
   const array = ['🍌', '🍇'];
-  const resultado = 'Resultado da operação unshift';
+  array.unshift("🍓");
+  const resultado = array;
   document.getElementById('resultado-unshift').textContent = resultado;
+  //Adiciona em primeiro o item no array.  
 }
 
 function executarIncludes() {
   const array =  ['🍎', '🍌', '🍇'];
-  const resultado = array;
+  const resultado = array.includes("🥝");
   document.getElementById('resultado-includes').textContent = resultado;
+  //Verifica se o array inclue certo objeto.
 }
 
 function executarReverse() {
   const array = ['🍎', '🍌', '🍇'];
+  array.reverse();
   const resultado = array;
   document.getElementById('resultado-reverse').textContent = resultado;
+  //Reverte a ordem dos itens.
 }
 
 function executarSort() {
-  const array = [3, 1, 4, 2];
+  const array = [3, 1, 4, 2, 11];
   const array2 = ["Brasil", "Argentina", "Noruega", "Italia"];
 
-  const resultado = array;
+  const resultado = array.sort((a,b) => a - b);
   document.getElementById('resultado-sort').textContent = resultado;
-  const resultado2 = array2;
+  const resultado2 = array2.sort();
   document.getElementById('resultado-sort2').textContent = resultado2;
+  //Sort em números entende q 1 vem na frente mesmo se for 11.
+  //Precisa ser usado uma função para fazer ele ordenar de forma correta.
+  //Inverter pode ser usado b - a além de a - b.
+
+  //Ordena os itens do array, string em ordem alfabetica/lexica.
+  //Pode usar sort().reverse(); para inverter e ordenar.
 }
 
 function executarSlice() {
@@ -73,56 +90,72 @@ function executarJoin() {
 
 function executarForEach() {
   const array = ['🍎', '🍌', '🍇'];
-  const resultado = 'Resultado da operação forEach';
+  const resultado = array.forEach((pegaItem) => console.log(pegaItem));
   document.getElementById('resultado-foreach').textContent = resultado;
+  //Passa por cada item no array. For each item.
 }
 
 function executarMap() {
   const array = ['🍎', '🍌', '🍇'];
-  const resultado = 'Resultado da operação map';
+  const resultado = array.map(pegaItem => `✅ ${pegaItem}`);
   document.getElementById('resultado-map').textContent = resultado;
+  //O mesmo que o ForEach mas retorna o mesmo com modificação.
 }
 
  function executarMapHTML() {
     const dispositivos = ['🎮', '🕹️', '💻'];
-    const resultado = 'Resultado da operação map inserindo HTML';
+    const resultado = dispositivos.map((pegaItem, index) => {
+      return `<h1> Item em promoção</h1>
+      <p> ${pegaItem}</p>`
+    });
     document.getElementById('resultado-map-html').innerHTML = resultado.join('<br>');
+    //Posso usar pra criar cards inteiros e pode ser pego mais de um parametro para pegar o index por exemplo.
   }
 
 function executarFilter() {
-  const array = ['🍎', '🍌', '🍇'];
-  const resultado = 'Resultado da operação filter';
+  const array = ['🍎', '🍌', '🍇' ,'🍎', '🍌', '🍇'];
+  const resultado = array.filter(pegaItem => pegaItem ==='🍎');
   document.getElementById('resultado-filter').textContent = resultado;
+  //Retorna um novo array com algum tipo de filtro ou condição
 }
 
 function executarFind() {
   const array =  ['🍎', '🍌', '🍇'];
-  const resultado = 'Resultado da operação find';
+  const resultado = array.find(pegaItem => pegaItem === '🍎');
   document.getElementById('resultado-find').textContent = resultado;
+  //Procura algo dentro de um array, retorna um True ou False, quase um includes mas pode fazer procuras mais complexas
 }
 
 function executarFindIndex() {
   const array =  ['🍎', '🍌', '🍇'];
-  const resultado = 'Resultado da operação findIndex';
+  const resultado = array.findIndex(pegaItem => pegaItem === '🍎');
   document.getElementById('resultado-findIndex').textContent = resultado;
+  //Retorna a posição do objeto
 }
 
 function executarReduce() {
   const array = [1, 2, 3, 4];
-  const resultado = 'Resultado da operação reduce';
+  const resultado = array.reduce((totalAcumulado, valorAtual) => {
+    return totalAcumulado + valorAtual
+  },0);
   document.getElementById('resultado-reduce').textContent = resultado;
+  //Vai somar tudo que tem dentro, útil para um carrinho de compras por exemplo.
+
+  //,0 é para ter certeza q ele comece em 0 a conta.
 }
 
 function executarEvery() {
   const array = [10, 20, 30, 40];
   const resultado = 'Resultado da operação every';
   document.getElementById('resultado-every').textContent = resultado;
+  //Checa se todos os itens são de um tipo, como string ou int.
 }
 
 function executarSome() {
   const array = ['🍎', '🍌', '🥝'];
   const resultado = 'Resultado da operação some';
   document.getElementById('resultado-some').textContent = resultado;
+  //Se algum item segue algum tipo de método.
 }
 
 /* Encadeamento */
@@ -133,4 +166,8 @@ function executarEncadeamento() {
   document.getElementById("resultado-encadeamento").textContent = resultado.join(', ') || 'Nenhum jogo encontrado';
 }
 
-
+const ordernar = (a) => a - 2
+// //Exemplo de Arrow Fuction q é a mesma coisa q:
+function ordernar2(a){
+  return a - 2
+}
