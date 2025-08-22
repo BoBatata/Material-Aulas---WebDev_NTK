1ca104459868197de4226defac5a0e165a7c87ea
Nicolas Tetsuo Kimura <nicolas.t.kimura@gmail.com>
2025-08-21 22:42:10 -0300

    [Feature] Editar, Apagar, Salvar e Carregar posts.

--- Aula 3 - Local e Session Storage/Mini-Rede-Social-CreateERead/script.js
+++ Aula 3 - Local e Session Storage/Mini-Rede-Social-CreateERead/script.js
@@ -22,17 +22,31 @@ let posts = [
 
 // Inicialização
 window.onload = function() {
+    carregarPosts();
     displayPosts();
 
-    document.getElementById('postForm').addEventListener('submit', addPost); 
+    document.getElementById('postForm').addEventListener('submit', addPost);
+    document.querySelector('#postList').addEventListener('click', handleClick);
 };
+
+function handleClick(infosDoEvento) {
+    const action = infosDoEvento.target.dataset.action;
+    const index = infosDoEvento.target.dataset.index;
+
+    if(action === "Editar"){
+        editarPost(index);
+    }
+    else if (action === "Apagar"){
+        apagarPost(index);
+    }
+}
 
 // Função para exibir os posts
 function displayPosts() {
     const postList = document.getElementById('postList');
     postList.innerHTML = '';
 
-    posts.forEach(pegaPost => {
+    posts.forEach((pegaPost, index) => {
             const postElement = document.createElement('div');
             postElement.classList.add('card-post');
   
@@ -41,10 +55,10 @@ function displayPosts() {
                 ${pegaPost.image ? `<img src="${pegaPost.image}" alt="Imagem do post" style="max-width:150px;">` : ""}
                 <p><em>Categoria: ${pegaPost.category}</em></p>
                 <p><em>Data e Hora: ${pegaPost.date}</em></p>
-                <button><i class="fa-solid fa-pen-to-square"></i> Editar</button>
-                <button><i class="fa-solid fa-eraser"></i> Apagar</button>
+                <button data-action="Editar" data-index="${index}"><i class="fa-solid fa-pen-to-square"></i> Editar</button>
+                <button data-action="Apagar" data-index="${index}"><i class="fa-solid fa-eraser"></i> Apagar</button>
                 <hr style="margin:30px;">`;
-               
+                
             postList.append(postElement);
         });
 }
@@ -66,8 +80,38 @@ function addPost(event) {
     };
     
     posts.unshift(post);
-    
-    document.getElementById('postForm').reset();
+    salvarPosts();
+    document.querySelector('#postForm').reset();
     
     displayPosts();
+}
+
+
+function editarPost(index){
+    const novoTexto = prompt('Edite o seu post:', posts[index].text);
+    posts[index].text = novoTexto;
+    salvarPosts();
+    displayPosts();
+}
+
+function apagarPost(index){
+    const confirmar = confirm("Tem certeza?");
+    if(confirmar)
+    {
+        posts.splice(index, 1);
+        salvarPosts();
+    }
+    displayPosts();
+}
+
+function salvarPosts(){
+    localStorage.setItem("posts", JSON.stringify(posts));
+}
+
+function carregarPosts(){
+    const postGuardados = localStorage.getItem("posts");
+
+    if(postGuardados){
+        posts = JSON.parse(postGuardados);
+    }
 }
