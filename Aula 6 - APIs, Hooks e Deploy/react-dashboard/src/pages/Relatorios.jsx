1a97a63463babedfe4f3237db4a7a1ec0a6d7d7b
Nicolas Tetsuo Kimura <nicolas.t.kimura@gmail.com>
2025-09-25 22:37:41 -0300

    [Feature] Shadcn, Button and Chart

--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Relatorios.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Relatorios.jsx
@@ -1,8 +1,24 @@
+import { GraficoBarras } from "@/components/ui/GraficoBarras"
+import { GraficoRadar } from "@/components/ui/GraficoRadar"
+import { Button } from "@/components/ui/button"
+
 export default function Relatorios() {
+
+  function handleClick(){
+    alert("clicou!");
+  }
+
   return (
     <div className="p-6 flex-1 bg-gray-100">
       <h1 className="text-2xl font-bold">Relatórios</h1>
       <p className="mt-2 text-gray-600">Página de relatórios.</p>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button onClick={handleClick}>Click me</Button>
+        <div class="grid grid-cols-3">
+          <GraficoRadar/>
+          <GraficoBarras/>
+        </div>
+      </div>
     </div>
   );
 }
